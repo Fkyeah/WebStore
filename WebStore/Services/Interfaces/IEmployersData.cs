ac40ac7a3964bea5f7f624f14e33fc8ff91f02f1
Fkyeah <dimx63@mail.ru>
2021-09-25 16:57:20 +0300

    Настройка интерфейса

---
+++ WebStore/Services/Interfaces/IEmployersData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Model;
+
+namespace WebStore.Services.Interfaces
+{
+    interface IEmployersData
+    {
+        IEnumerable<Employer> GetAllEmployers();
+        Employer GetById();
+        int AddEmployer(Employer employer);
+        void UpdateEmployer(int id, Employer employer);
+        bool DeleteEmployer(int id);
+    }
+}
