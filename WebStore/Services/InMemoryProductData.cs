5f2a4a4cfccbf884d875c3cf4d8e167ff0ee9e52
Fkyeah <dimx63@mail.ru>
2021-09-26 21:28:25 +0300

    Контроллер каталога товаров

--- WebStore/Services/InMemoryProductData.cs
+++ WebStore/Services/InMemoryProductData.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Model;
 using WebStore.Services.Interfaces;
@@ -15,6 +17,16 @@ namespace WebStore.Services
         public IEnumerable<Brand> GetBrands()
         {
             return TestData.Brands;
+        }
+
+        public IEnumerable<Product> GetProducts(ProductFilter filter = null)
+        {
+            IEnumerable<Product> products = TestData.Products;
+            if (filter?.SectionId != null)
+                products = products.Where(el => el.SectionId == filter.SectionId);
+            if (filter?.BrandId != null)
+                products = products.Where(el => el.BrandId == filter.BrandId);
+            return products;
         }
     }
 }
---
+++ WebStore/ViewModels/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.ViewModels
+{
+    public class ProductViewModel : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+        public int SectionId { get; set; }
+        public int? BrandId { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
