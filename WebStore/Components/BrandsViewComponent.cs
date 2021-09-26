2f125443b92975692411c7e9991035ce34409661
Fkyeah <dimx63@mail.ru>
2021-09-26 19:30:50 +0300

    Настройка компонентов и представлений для отображения брендов и категорий

--- WebStore/Components/BrandsViewComponent.cs
+++ WebStore/Components/BrandsViewComponent.cs
@@ -3,14 +3,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Components
 {
     public class BrandsViewComponent : ViewComponent
     {
+        private readonly IProductData _productData;
+
+        public BrandsViewComponent(IProductData productData)
+        {
+            _productData = productData;
+        }
         public IViewComponentResult Invoke()
         {
-            return View();
+            var brands = _productData.GetBrands();
+            var brandsViews = brands
+                .OrderByDescending(el => el.Order)
+                .Select(el => new BrandViewModel
+                {
+                    Id = el.Id,
+                    Name = el.Name,
+                    Order = el.Order,
+                })
+                .ToList();
+            return View(brandsViews);
         }
     }
 }
--- WebStore/Components/SectionsViewComponent.cs
+++ WebStore/Components/SectionsViewComponent.cs
@@ -3,14 +3,49 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Components
 {
     public class SectionsViewComponent : ViewComponent
     {
+        private readonly IProductData _productData;
+
+        public SectionsViewComponent(IProductData productData)
+        {
+            _productData = productData;
+        }
         public IViewComponentResult Invoke()
         {
-            return View();
+            var sections = _productData.GetSections();
+            var parentSections = sections.Where(el => el.ParentId is null);
+            var parentSectionsViews = parentSections
+                .Select(el => new SectionViewModel
+                {
+                    Id = el.Id,
+                    Name = el.Name,
+                    Order = el.Order,
+                })
+                .ToList();
+
+            foreach(var parent in parentSectionsViews)
+            {
+                var childs = sections.Where(q => q.ParentId == parent.Id);
+                foreach (var child in childs)
+                {
+                    parent.Childs.Add(new SectionViewModel
+                    {
+                        Id = child.Id,
+                        Name = child.Name,
+                        Order = child.Order,
+                        Parent = parent,
+                    }); ;
+                }
+                parent.Childs.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+            }
+            parentSectionsViews.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+            return View(parentSectionsViews);
         }
     }
 }
