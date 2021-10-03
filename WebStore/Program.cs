72983c155e98523b7bc5584195d0623ceaa39715
Fkyeah <dimx63@mail.ru>
2021-10-03 13:08:43 +0300

    Настройка запуска инициализатора

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using WebStore.Model;
 
 namespace WebStore
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var hostBuilder = CreateHostBuilder(args);
+            var host = hostBuilder.Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var initializer = scope.ServiceProvider.GetRequiredService<WebStoreDBInitializer>();
+                await initializer.InitializeAsync();
+            }
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
