a0b4fa569a6c685904d2e49287ce912f7bf68aab
Fkyeah <dimx63@mail.ru>
2021-10-03 13:23:19 +0300

    Запуск инициализатора базы данных

--- WebStore/Model/WebStoreDBInitializer.cs
+++ WebStore/Model/WebStoreDBInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.DAL.Context;
@@ -8,10 +9,12 @@ namespace WebStore.Model
     public class WebStoreDBInitializer
     {
         private readonly WebStoreDB _db;
+        private readonly ILogger<WebStoreDBInitializer> _logger;
 
-        public WebStoreDBInitializer(WebStoreDB db)
+        public WebStoreDBInitializer(WebStoreDB db, ILogger<WebStoreDBInitializer> logger)
         {
             _db = db;
+            _logger = logger;
         }
         public async Task InitializeAsync()
         {
@@ -19,12 +22,18 @@ namespace WebStore.Model
             var appliedMigrations = await _db.Database.GetAppliedMigrationsAsync();
 
             if (pendingMigartions.Any())
+            {
+                _logger.LogInformation("Запуск миграций..");
                 await _db.Database.MigrateAsync();
+            }
+                
 
             await InitializeProductAsync();
+            _logger.LogInformation("Инициализация продуктов..");
         }
         private async Task InitializeProductAsync()
         {
+            _logger.LogInformation("Добавление брендов..");
             await using(await _db.Database.BeginTransactionAsync())
             {
                 _db.Brands.AddRange(TestData.Brands);
@@ -33,14 +42,8 @@ namespace WebStore.Model
                 await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Brands] ON");
                 await _db.Database.CommitTransactionAsync();
             }
-            await using (await _db.Database.BeginTransactionAsync())
-            {
-                _db.Products.AddRange(TestData.Products);
-                await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Products] ON");
-                await _db.SaveChangesAsync();
-                await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Products] ON");
-                await _db.Database.CommitTransactionAsync();
-            }
+            _logger.LogInformation("Добавление брендов прошло успешно..");
+            _logger.LogInformation("Добавление секций..");
             await using (await _db.Database.BeginTransactionAsync())
             {
                 _db.Sections.AddRange(TestData.Sections);
@@ -49,6 +52,17 @@ namespace WebStore.Model
                 await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Sections] ON");
                 await _db.Database.CommitTransactionAsync();
             }
+            _logger.LogInformation("Добавление секций прошло успешно..");
+            _logger.LogInformation("Добавление продуктов..");
+            await using (await _db.Database.BeginTransactionAsync())
+            {
+                _db.Products.AddRange(TestData.Products);
+                await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Products] ON");
+                await _db.SaveChangesAsync();
+                await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Products] ON");
+                await _db.Database.CommitTransactionAsync();
+            }
+            _logger.LogInformation("Добавление продуктов прошло успешно..");
         }
     }
 }
