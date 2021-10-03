afee3c27b336426b3026f9298a5c79540d91cc6c
Fkyeah <dimx63@mail.ru>
2021-10-03 12:02:36 +0300

    Создание структуры базы данных

--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,10 +1,13 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities
 {
+    [Table("Brands")]
     public class Brand : NamedEntity, IOrderedEntity
     {
+        [Column("Brand_order_id")]
         public int Order { get; set; }
     }
 }
--- WebStore.Domain/Entities/Section.cs
+++ WebStore.Domain/Entities/Section.cs
@@ -1,11 +1,21 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities
 {
+    [Table("Products")]
     public class Section : NamedEntity, IOrderedEntity
     {
+        [Column("Section_order_id")]
         public int Order { get; set; }
+        
         public int? ParentId { get; set; }
+        
+        [ForeignKey(nameof(ParentId))]
+        public Section Parent { get; set; }
+        
+        public ICollection<Product> Products { get; set; }
     }
 }
