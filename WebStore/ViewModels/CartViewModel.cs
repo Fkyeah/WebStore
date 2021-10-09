42225cd7f6e1f9b2f42029feaa87b8413899ff2f
Fkyeah <dimx63@mail.ru>
2021-10-09 21:26:24 +0300

    Настройка ViewModel корзины

--- WebStore/ViewModels/CartViewModel.cs
+++ WebStore/ViewModels/CartViewModel.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WebStore.ViewModels
 {
     public class CartViewModel
     {
-        
+        public IEnumerable<(ProductViewModel product, int quantity)> Items { get; set; }
+
+        public int ItemsCount => Items?.Sum(item => item.quantity) ?? 0;
+        public decimal TotalSum => Items?.Sum(item => item.product.Price * item.quantity) ?? 0;
     }
 }
