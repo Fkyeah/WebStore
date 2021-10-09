dc3d722d126147687a596564745bb7bd68856000
Fkyeah <dimx63@mail.ru>
2021-10-09 20:55:10 +0300

    Реализация сервиса

---
+++ WebStore/Services/InCookies/CartService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Services.InCookies
+{
+    public class CartService : ICartService
+    {
+        private readonly HttpContextAccessor _httpContextAccessor;
+        private readonly IProductData _productData;
+        private readonly string _cartName;
+
+        public CartService(HttpContextAccessor httpContextAccessor, IProductData productData)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _productData = productData;
+
+            var user = httpContextAccessor.HttpContext.User;
+            var userName = user.Identity!.IsAuthenticated ? $"-{user.Identity.Name}" : null;
+            _cartName = $"Webstore.Cart{userName}";
+        }
+        public void Add(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Decrement(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public CartViewModel GetViewModel()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
