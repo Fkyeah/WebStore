31e23519be58e68e1487078b31d5ab95f9414ac6
Fkyeah <dimx63@mail.ru>
2021-10-10 12:39:22 +0300

    Админка. Контроллер каталога продуктов

---
+++ WebStore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IProductData _productData;
+
+        public ProductController(IProductData productData)
+        {
+            _productData = productData;
+        }
+        public IActionResult Index()
+        {
+            var products = _productData.GetProducts();
+            return View(products);
+        }
+    }
+}
