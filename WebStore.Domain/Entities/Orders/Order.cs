f55f59ba02c076915da8afb94cbae97a8a925b54
Fkyeah <dimx63@mail.ru>
2021-10-10 13:16:43 +0300

    Сущность заказа

---
+++ WebStore.Domain/Entities/Orders/Order.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base;
+
+namespace WebStore.Domain.Entities.Orders
+{
+    public class Order : BaseEntity
+    {
+        [Required]
+        public User User { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Phone { get; set; }
+        
+        [Required]
+        [MaxLength(200)]
+        public string Address { get; set; }
+        
+        public string Description { get; set; }
+        
+        public DateTimeOffset Date { get; set; } = DateTimeOffset.UtcNow;
+        
+        public IEnumerable<OrderItem> Items { get; set; } = new List<OrderItem>();
+    }
+    public class OrderItem : BaseEntity
+    {
+        [Required]
+        public Product Product { get; set; }
+        
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        
+        public Order Order { get; set; }
+        
+        [NotMapped]
+        public decimal TotalPrice => Price * Quantity;
+        
+        public int Quantity { get; set; }
+    }
+}
