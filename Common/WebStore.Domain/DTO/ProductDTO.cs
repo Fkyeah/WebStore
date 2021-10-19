24912963c83eb285e16948a558ed6dec55b1ad66
Fkyeah <dimx63@mail.ru>
2021-10-19 21:31:34 +0300

    Разделение DTO моделей

--- Common/WebStore.Domain/DTO/ProductDTO.cs
+++ Common/WebStore.Domain/DTO/ProductDTO.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WebStore.Domain.Entities;
 
 namespace WebStore.Domain.DTO
@@ -18,70 +15,7 @@ namespace WebStore.Domain.DTO
         public SectionDTO Section { get; set; }
 
     }
-
-    public class BrandDTO
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Order { get; set; }
-    }
-    public class SectionDTO
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Order { get; set; }
-        public int? ParentId { get; set; }
-    }
-    public static class BrandDTOMapper
-    {
-        public static BrandDTO ToDTO(this Brand brand) => brand is null
-            ? null
-            : new BrandDTO
-            {
-                Id = brand.Id,
-                Name = brand.Name,
-                Order = brand.Order,
-            };
-
-        public static Brand FromDTO(this BrandDTO brand) => brand is null
-            ? null
-            : new Brand
-            {
-                Id = brand.Id,
-                Name = brand.Name,
-                Order = brand.Order,
-            };
-
-        public static IEnumerable<BrandDTO> ToDTO(this IEnumerable<Brand> brands) => brands.Select(ToDTO);
-        public static IEnumerable<Brand> FromDTO(this IEnumerable<BrandDTO> brands) => brands.Select(FromDTO);
-    }
-
-    public static class SectionDTOMapper
-    {
-        public static SectionDTO ToDTO(this Section section) => section is null
-            ? null
-            : new SectionDTO
-            {
-                Id = section.Id,
-                Name = section.Name,
-                Order = section.Order,
-                ParentId = section.ParentId,
-            };
-
-        public static Section FromDTO(this SectionDTO section) => section is null
-            ? null
-            : new Section
-            {
-                Id = section.Id,
-                Name = section.Name,
-                Order = section.Order,
-                ParentId = section.ParentId,
-            };
-
-        public static IEnumerable<SectionDTO> ToDTO(this IEnumerable<Section> sections) => sections.Select(ToDTO);
-        public static IEnumerable<Section> FromDTO(this IEnumerable<SectionDTO> sections) => sections.Select(FromDTO);
-    }
-
+    
     public static class ProductDTOMapper
     {
         public static ProductDTO ToDTO(this Product product) => product is null
@@ -113,6 +47,6 @@ namespace WebStore.Domain.DTO
             };
 
         public static IEnumerable<ProductDTO> ToDTO(this IEnumerable<Product> products) => products.Select(ToDTO);
-
         public static IEnumerable<Product> FromDTO(this IEnumerable<ProductDTO> products) => products.Select(FromDTO);
     }
+}
