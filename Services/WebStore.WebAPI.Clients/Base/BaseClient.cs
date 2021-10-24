0dbd61f04a77ce20b5918cd14f46de571ce44b26
Fkyeah <dimx63@mail.ru>
2021-10-24 15:41:57 +0300

    IDisposable для базового клиента

--- Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
+++ Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace WebStore.WebAPI.Clients.Base
 {
-    public abstract class BaseClient
+    public abstract class BaseClient : IDisposable
     {
         protected HttpClient _httpClient { get; } 
         protected string _controllerAddress { get; }
@@ -48,6 +48,26 @@ namespace WebStore.WebAPI.Clients.Base
         {
             var response = await _httpClient.DeleteAsync(url).ConfigureAwait(false);
             return response;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            //GC.SuppressFinalize(this);
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_Disposed) return;
+            _Disposed = true;
+
+            if (disposing)
+            {
+                // должны освободить управляемые ресурсы
+            }
+
+            // должны освободить неуправляемые ресурсы
         }
     }
 }
