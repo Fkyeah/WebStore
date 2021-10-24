2cdf10058028b303ca1081a311f697ce7e3f4f8c
Fkyeah <dimx63@mail.ru>
2021-10-24 15:04:42 +0300

    Заготовка контроллеров системы Identity

---
+++ Services/WebStore.WebAPI/Controllers/Identity/RolesApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities;
+
+namespace WebStore.WebAPI.Controllers.Identity
+{
+    [Route(WebStore.Interfaces.WebAPI.Roles)]
+    [ApiController]
+    public class RolesApiController : ControllerBase
+    {
+        private readonly RoleStore<Role> _roleStore;
+        public RolesApiController(WebStoreDB db)
+        {
+            _roleStore = new RoleStore<Role>(db);
+        }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<Role>> GetAll() => await _roleStore.Roles.ToArrayAsync();
+    }
+}
---
+++ Services/WebStore.WebAPI/Controllers/Identity/UsersApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities;
+
+namespace WebStore.WebAPI.Controllers.Identity
+{
+    [Route(WebStore.Interfaces.WebAPI.Users)]
+    [ApiController]
+    public class UsersApiController : ControllerBase
+    {
+        private readonly UserStore<User, Role, WebStoreDB> _userStore;
+        public UsersApiController(WebStoreDB db)
+        {
+            _userStore = new UserStore<User, Role, WebStoreDB>(db);
+        }
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<User>> GetAll() => await _userStore.Users.ToArrayAsync();
+    }
+}
