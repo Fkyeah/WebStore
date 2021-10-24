39baf24a1d75f75a778b2f7102853d97b3cbe5bf
Fkyeah <dimx63@mail.ru>
2021-10-24 14:03:26 +0300

    Интерфейсы сервиса Identity

---
+++ Services/WebStore.Interfaces/Services/Identity/IUsersClient.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Interfaces.Services.Identity
+{
+    public interface IUsersClient :
+        IUserStore<User>,
+        IUserPasswordStore<User>,
+        IUserEmailStore<User>,
+        IUserPhoneNumberStore<User>,
+        IUserLoginStore<User>,
+        IUserTwoFactorStore<User>,
+        IUserClaimStore<User>
+    {
+        
+    }
+}
