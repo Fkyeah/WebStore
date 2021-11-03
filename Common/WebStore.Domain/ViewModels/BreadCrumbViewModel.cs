3f9eb4006833d25329c7c212795c372d5bb82164
Fkyeah <dimx63@mail.ru>
2021-11-03 20:54:08 +0300

    Хлебные крошки

---
+++ Common/WebStore.Domain/ViewModels/BreadCrumbViewModel.cs
@@ -0,0 +1,11 @@
+﻿using WebStore.Domain.Entities;
+
+namespace WebStore.Domain.ViewModels
+{
+    public class BreadCrumbViewModel
+    {
+        public Brand Brand { get; set; }
+        public Section Section { get; set; }
+        public string Product { get; set; }
+    }
+}
---
+++ UI/WebStore/Components/BreadCrumbsViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Components
+{
+    public class BreadCrumbsViewComponent : ViewComponent
+    {
+        private readonly IProductData _productData;
+
+        public BreadCrumbsViewComponent(IProductData productData)
+        {
+            _productData = productData;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var model = new BreadCrumbViewModel();
+            
+            if(int.TryParse(Request.Query["BrandId"], out var brandId))
+            {
+                model.Brand = _productData.GetBrandById(brandId);
+            }    
+
+            if(int.TryParse(Request.Query["SectionId"], out var sectionId))
+            {
+                model.Section = _productData.GetSectionById(sectionId);
+                if(model.Section.ParentId is { } parentSectionId)
+                {
+                    model.Section.Parent = _productData.GetSectionById(parentSectionId);
+                }
+            }
+
+            if(int.TryParse(ViewContext.RouteData.Values["id"]?.ToString(), out var productId))
+            {
+                model.Product = _productData.GetProductById(productId)?.Name;
+            }
+
+            return View(model);
+        }
+    }
+}
